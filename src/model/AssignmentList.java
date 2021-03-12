0b8013e7d4719c02041a20f5b7935ffc3e86281f
owentangjintanakarn <owen.t95@hotmail.com>
2021-03-12 19:51:54 +0700

    Initial Implementation

--- src/model/AssignmentList.java
+++ src/model/AssignmentList.java
@@ -12,6 +12,39 @@ public class AssignmentList extends AbstractList implements Serializable {
 
     }
 
+    public void addAssignment(AssignmentInterface a) {
+        /*TODO: add check to see if assignment already exists.
+            Validate by comparing name and due date (helper method)
+            else, throw AssignmentAlreadyExists Exception
+         */
+        aList.add(a);
+    }
+
+    public void removeAssignment(AssignmentInterface a) {
+        /*
+        TODO: check to see if assignment is available for removal
+         */
+        aList.remove(a);
+    }
+
+    public boolean doesAssignmentExist(AssignmentInterface a) {
+        boolean bool = false;
+        if (aList.contains(a)) {
+            bool = true;
+        }
+        return bool;
+    }
+
+    public void getList(){
+        if (aList.isEmpty()) {
+            System.out.println("List is empty");
+        }
+
+        for (AssignmentInterface a : aList) {
+            System.out.println();
+        }
+    }
+
     @Override
     public int size() {
         return aList.size();
